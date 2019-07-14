1cf2b2ea3bbf5eb0ba1848868b5eb15c12a51be7
Unknown <okhick@gmail.com>
2019-07-14 19:32:11 -0400

    Added jsdoc to keep things organized

--- src/db_functions.js
+++ src/db_functions.js
@@ -8,13 +8,19 @@ class DBQuery {
     //nothing for now
   }
 
+  /**
+   * async newVideo - Adds a new video to the database
+   *
+   * @param  {string} google_id
+   * @return {int}              the ID of the row created
+   */
   async newVideo(google_id) {
-    let videoExists = await this.checkExists("videos", "google_id", google_id);
+    let videoExists = await this._checkExists("videos", "google_id", google_id);
 
     if (!videoExists) {
       let insert = "INSERT INTO videos (google_id, date_added) VALUES ($google_id, date('now'))";
       return new Promise( (resolve, reject) => {
-        db.run(insert, { $google_id: google_id }, function(err) {
+        db.run(insert, { $google_id:google_id }, function(err) {
           if (err) {
             reject(err);
           } else {
@@ -29,15 +35,23 @@ class DBQuery {
     }
   }
 
-  checkExists(table, column, value) {
+  /**
+   * _checkExists - Quick helper function to check a table for a value.
+   *
+   * @param  {string}          table  the table you want to check
+   * @param  {string}          column the column you want to check
+   * @param  {string | int}    value  the value you want to check for
+   * @return {boolean}                true if exists, false if doesn't exist
+   */
+  _checkExists(table, column, value) {
     let select = `SELECT EXISTS (SELECT * FROM ${table} WHERE ${column} = $value)`;
 
     return new Promise( (resolve, reject) => {
       db.get(select, { $value:value }, (err, row) => {
         if (err) { reject(err); }
-        if (row) {
+        if (row) { //row is an object formatted as {query:result}
           let key = Object.keys(row);
-          let exists = (row[key] == 1 ? true : false);
+          let exists = (row[key] == 1) ? true : false;
           resolve(exists);
         }
       });
@@ -48,17 +62,16 @@ class DBQuery {
 db.on("open", async () => {
   const query = new DBQuery();
   let nextRow = await query.newVideo('fD-SWaIT8uk');
-  console.log(nextRow);
+  quickSelect();
 });
 
-// function quickSelect() {
-//   let select = "SELECT * from videos";
-//   db.each(select, {}, (err, row) => {
-//     if (err) { console.log(err) };
-//     if (row) { console.log(row) };
-//   });
-//
-// }
+function quickSelect() {
+  let select = "SELECT * from videos";
+  db.each(select, {}, (err, row) => {
+    if (err) { console.log(err) };
+    if (row) { console.log(row) };
+  });
+}
 //
 // function quickUpdate() {
 //   let update = "UPDATE videos SET google_id = $google_id WHERE google_id = $badGoogleID";
--- src/google_functions.js
+++ src/google_functions.js
@@ -1,12 +1,14 @@
 const {google} = require('googleapis');
 const fs = require('fs');
 
+// Load up from the creds json with Google API key and info.
 const creds = ( () => {
     let credsRaw = fs.readFileSync('creds.json');
     return JSON.parse(credsRaw)
   }
 )();
 
+//Set up the class with creds
 const youtube = google.youtube({
    version: creds.version,
    auth: creds.auth
@@ -21,6 +23,11 @@ class CommentQuery {
     };
   }
 
+  /**
+   * async getComments - Request a comments list from YouTube and parses them into an array
+   *
+   * @return {Array} An array of comments objects
+   */
   async getComments() {
     try{
       this.rawComments = await this._getRawComments();
@@ -34,6 +41,11 @@ class CommentQuery {
     }
   }
 
+  /**
+   * _getRawComments - The actual request to YouTube
+   *
+   * @return {Object} A full object with lots of data about and including comments
+   */
   _getRawComments() {
     return new Promise ((resolve, reject) => {
       youtube.commentThreads.list(this.videoArgs, (err, res) => {
@@ -43,6 +55,12 @@ class CommentQuery {
     });
   }
 
+  /**
+   * _drillDownToComments - gets only the comment data and returns an array of objects.
+   *
+   * @param  {Object} rawComments the full response from YouTube returned by _getRawComments
+   * @return {Array}              An array with data for each comment
+   */
   _drillDownToComments(rawComments) {
     let comments = [];
     rawComments.data.items.forEach( (comment) => {
